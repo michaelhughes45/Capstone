423d57399863ae1a7f1c4c61e1f9438d6052ebb5
Michael Hughes <mhughesy23@gmail.com>
2025-03-11 11:53:14 -0600

    added the person functions to db.js

---
+++ backend/routes/people.js
@@ -0,0 +1,20 @@
+Person = require('../models/person')
+const DBWrapper = require('./db')
+var express = require('express')
+var router = express.Router()
+
+// addPerson()
+
+// deletePerson()
+
+// getAllPeople()
+
+// getPersonById()
+
+// getPersonByUsername()
+
+// getUnitsOwned()
+
+// getUnitsStayedIn()
+
+// updatePerson()
